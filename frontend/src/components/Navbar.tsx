334801a4d32a69461ef14ea9123417dfe5c110aa
andrewfurey21 <andrewfurey2003@gmail.com>
2023-06-09 16:59:04 +0100

    added navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import "../styles/main.scss"
+import BookIcon from "../assets/book.svg";
+import VideoIcon from "../assets/video.svg";
+import ProfileIcon from "../assets/profile.svg";
+
 
 export function Navbar() {
     return (
         <div className="navbar">
+            <div className="logo">
+                <a href="#">glotalot</a>
+            </div>
+
+            <div className="activities">
+               <div>
+                   <img className="activitiesImage" src={BookIcon} alt="book"/>
+               </div>
+               <div>
+                   <img className="activitiesImage" src={VideoIcon} alt="video"/>
+               </div>
+            </div>
+
+            <div>
+                <img className="profileIcon" src={ProfileIcon} alt=""/>
+            </div>
         </div>
     );
 }
-
