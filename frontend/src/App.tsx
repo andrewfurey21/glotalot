6416b1bfd57ca0ac1eb8f8d84e7382d6525cce39
andrewfurey21 <andrewfurey2003@gmail.com>
2023-06-10 15:58:24 +0100

    styles for text header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import "./styles/main.scss"
-import { Navbar } from './components/Navbar'
+import React from 'react';
+import "./styles/main.scss";
+import { Navbar } from './components/Navbar';
+import { TextHeader } from './components/TextHeader'
 
 
 function App() {
   return (
     <div className="app">
       <Navbar/>
+      <TextHeader/>
     </div>
   );
 }
