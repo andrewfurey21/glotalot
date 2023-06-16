290088b39d1626c28f275fcbc75df3291fbe70c6
andrewfurey21 <andrewfurey2003@gmail.com>
2023-06-16 21:47:40 +0100

    connected frontend to backend with axios

--- frontend/src/components/TextsPage.tsx
+++ frontend/src/components/TextsPage.tsx
@@ -1,8 +1,21 @@
 
 import { NoTexts } from "./NoTexts";
 import { TextHeader } from './TextHeader';
+import axios from 'axios'
 
 export function TextsPage() {
+    const getTextDescriptions = async() => {
+        axios.get('/texts')
+             .then(function (response) {
+                 console.log(response);
+             })
+             .catch(function (error) {
+                 console.log(error);
+             })
+      };
+
+    getTextDescriptions();
+
     return (
         <div className="TextsPage">
             <TextHeader/>
