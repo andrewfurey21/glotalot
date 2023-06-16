1228d1c618ca09cb4f675866d2d5a5bda21e6a30
andrewfurey21 <andrewfurey2003@gmail.com>
2023-06-16 19:02:30 +0100

    cleaned up

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,38 @@
-import redis
+from fastapi import FastAPI
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
+
+middlewares = [
+     Middleware(
+         CORSMiddleware,
+         allow_origins=['*'],
+         allow_methods=['*'],
+         allow_headers=['*']
+     )
+ ]
+
+app = FastAPI(middleware=middlewares)
+
+# with open('../../../italia/anello.txt') as f:
+#     lines = f.readlines()
+#
+# client = MongoClient('localhost', 27017)
+
+# db = client.test_database
+#
+# article = {"title": "La Compagna dell'Anello", "text": lines}
+#
+# articles = db.articles
+#
+# articles.insert_one(article)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/create")
+async def create_text():
+    return {"message": "creating text"}
+
+async def read_text():
+    pass
