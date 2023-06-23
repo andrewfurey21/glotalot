4a093b0f17a1d9e221c0cf7b3d010026930dbd9c
andrewfurey21 <andrewfurey2003@gmail.com>
2023-06-23 21:58:41 +0100

    added translation

--- backend/translate.py
+++ backend/translate.py
@@ -1,1 +1,9 @@
-import deepl
+from deepl import Translator
+
+from key import *
+
+translator = Translator(auth_key)
+
+result = translator.translate_text("What are you doing?", target_lang="IT")
+
+print(result)
