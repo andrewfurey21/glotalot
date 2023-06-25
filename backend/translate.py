a659d0ba9cdbb76caac51085d5e290a62e89e5c5
andrewfurey21 <andrewfurey2003@gmail.com>
2023-06-25 22:06:33 +0100

    added source lang

--- backend/translate.py
+++ backend/translate.py
@@ -1,8 +1,8 @@
 from deepl import Translator
 from key import *
 
-def translate(text: str, target_lang="EN-GB"):
-    translation = Translator(auth_key).translate_text(text, target_lang=target_lang)
+def translate(text: str, target_lang="EN-GB", source_lang="IT"):
+    translation = Translator(auth_key).translate_text(text, source_lang=source_lang, target_lang=target_lang)
     return translation.text
 
 if __name__ == "__main__":
