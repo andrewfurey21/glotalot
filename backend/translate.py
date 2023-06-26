29a5c0139b93def24b0936c5dc5f1cf1d3f12d85
andrewfurey21 <andrewfurey2003@gmail.com>
2023-06-26 16:22:07 +0100

    added source_langs and target_langs that list available languages

--- backend/translate.py
+++ backend/translate.py
@@ -1,10 +1,24 @@
 from deepl import Translator
 from key import *
 
+def source_langs():
+    langs = Translator(auth_key).get_source_languages()
+    codes = []
+    for lang in langs:
+        codes.append({"lang": lang.name, "code": lang.code})
+    return codes
+
+def target_langs():
+    langs = Translator(auth_key).get_target_languages()
+    codes = []
+    for lang in langs:
+        codes.append({"lang": lang.name, "code": lang.code})
+    return codes
+
 def translate(text: str, target_lang="EN-GB", source_lang="IT"):
     translation = Translator(auth_key).translate_text(text, source_lang=source_lang, target_lang=target_lang)
     return translation.text
 
 if __name__ == "__main__":
-    translation = translate("hello")
-    print(translation)
+    # translation = translate("buongiorno")
+    print(target_langs())
