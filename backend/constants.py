3279ce8aeaee497df840331cce9ffefdfa42cea4
andrewfurey21 <andrewfurey2003@gmail.com>
2023-06-26 16:23:51 +0100

    added constants for the saved words

--- backend/constants.py
+++ backend/constants.py
@@ -3,3 +3,6 @@ PORT = 27017
 
 TEXT_COLLECTION_NAME = 'text_collection'
 TEXT_DATABASE_NAME = 'text_database'
+
+SAVED_WORDS_COLLECTION_NAME = 'saved_words_collection'
+SAVED_WORDS_DATABASE_NAME = 'saved_words_database'
