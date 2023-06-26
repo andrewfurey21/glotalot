f194d583032bcfe9a48eb7bec4b1847ba5b9c515
andrewfurey21 <andrewfurey2003@gmail.com>
2023-06-26 16:25:39 +0100

    added word bank to side of page

--- frontend/src/pages/Read.tsx
+++ frontend/src/pages/Read.tsx
@@ -11,35 +11,59 @@ interface Text {
     id: string,
 }
 
-async function getText(word: string) {
-    console.log("word: " + word)
-    let text = await axios.get('http://localhost:8000/translate?text='+word);
-    console.log(text)
-    return text;
+interface Translation {
+    word: string,
+    translation: string
+}
+
+function TranslationCard(word: string, translation: string, index: number) {
+    return (
+        <div className="translationCard" key={index}>
+            <h3 className="word">{word}</h3>
+            <h3 className="translation">{translation}</h3>
+        </div>
+    )
 }
 
 export function Read() {
     let { id } = useParams();
     let [text, setText] = useState<Text>({title: '', text: '', words: 0, completion_perc: 0, id: ''});
+    let [translations, setTranslations] = useState<Translation[]>([])
+
     useEffect(() => {
         (async () => {
-            let textInfo = await axios.get('http://localhost:8000/read?id='+id);
+            let textInfo = await axios.get('http://localhost:8000/read/?id='+id);
             setText(textInfo.data);
         })();
     }, [id]);
 
     return (
-        <div className="textDisplay">
-            <h1>Title: {text.title}</h1>
-            <h3> Word count: {text.words}</h3>
-            <p>{text.text}</p>
-            <div>{
-                text.text.split(" ").map((word, index) => {
-                    return (<button key={index} className="textButton" onClick={() => getText(word)}>
-                        {word}
-                    </button>)
-                })
-            }</div>
+        <div className="readDisplay">
+            <div className="textDisplay">
+                <h1 className="title">{text.title}</h1>
+                <div className="text">{
+                    text.text.split(" ").map((word, index) => {
+                        return (
+                            <span key={index} className="textButton" onClick={async () => {
+
+                                let text = await axios.get('http://localhost:8000/add_word/?text='+word);
+                                setTranslations([...translations, {word: word, translation: text.data}])
+                            }}>
+                                {word + " "}
+                            </span>
+                        )
+                    })
+                }</div>
+            </div>
+            <div className="wordBank">
+                {
+                    translations.map((translation, index) => {
+                        return (
+                            TranslationCard(translation.word, translation.translation, index)
+                        )
+                    })
+                }
+            </div>
         </div>
     );
 }
