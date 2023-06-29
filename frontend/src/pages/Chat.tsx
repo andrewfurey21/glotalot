92d160810c3c12b615bcc4404bd0084bc05d3970
andrewfurey21 <andrewfurey2003@gmail.com>
2023-06-29 15:54:06 +0100

    added chatbot ui to chat page

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,8 +1,44 @@
+import "../styles/main.scss"
+import { useState } from 'react'
+import axios from 'axios'
+
+function ChatCard(text: string, index: number) {
+    return (
+        <div className="chatBotTextCard" key={index}>
+            <p className="chatBotText">{text}</p>
+        </div>
+    )
+}
 
 export function Chat() {
+
+    let [question, setQuestion] = useState<string>("");
+    let [chats, setChats] = useState<string[]>([]);
+
+    const sendChat = async () => {
+        if (question !== "") {
+            let chatInfo = await axios.get("http://localhost:8000/chat/?question="+question);
+            console.log(chatInfo.data)
+            setChats([...chats, chatInfo.data])
+        }
+    }
+
     return (
-        <>
-            <p>Practice chatting in your target language with a chatbot.</p>
-        </>
+        <div className="chat">
+            <div className="chatInput">
+                <input placeholder="" value={question} className="chatInput" onChange={e => setQuestion(e.target.value)}/>
+                <button onClick={sendChat} className="sendChat">Send</button>
+            </div>
+            <div className="chats">
+                {
+                    chats.map((text, index) => {
+                        return (
+                            ChatCard(text, index)
+                        )
+                    })
+                }
+            </div>
+
+        </div>
     )
 }
