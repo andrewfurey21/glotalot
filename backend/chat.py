1f8e18e781e18b6ddfdcf003c37e9f57839deaf5
andrewfurey21 <andrewfurey2003@gmail.com>
2023-07-06 15:26:13 +0100

    started working with youtube api:

--- backend/chat.py
+++ backend/chat.py
@@ -4,22 +4,23 @@ from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.memory import ConversationBufferWindowMemory
 
-from constants import MODEL_PATH
+from constants import *
 
-template = """
-Only speak {language}
-"""
+if LLM_ENABLED:
+    template = """
+    Only speak {language}
+    """
+    prompt = PromptTemplate.from_template(template)
+    prompt.format(language="italian")
 
-prompt = PromptTemplate.from_template(template)
-prompt.format(language="italian")
+    llm = LlamaCpp(
+        model_path=MODEL_PATH, verbose=False
+    )
 
-#callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
-
-llm = LlamaCpp(
-    model_path=MODEL_PATH, verbose=False
-)
-
-llm_chain = LLMChain(prompt=prompt, llm=llm)
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
 
 def ask_question(question: str):
-    return llm_chain.run(question)
+    if LLM_ENABLED:
+        return llm_chain.run(question)
+    else:
+        return "Chatbot not available."
--- backend/constants.py
+++ backend/constants.py
@@ -8,3 +8,4 @@ SAVED_WORDS_COLLECTION_NAME = 'saved_words_collection'
 SAVED_WORDS_DATABASE_NAME = 'saved_words_database'
 
 MODEL_PATH = "../../models/ggml-vicuna-7b-1.1-q4_0.bin"
+LLM_ENABLED = False
